cf7bc034e44ebd292a03435119a615fcf4d1d74e
chenl <18116491546@163.com>
2023-06-29 21:58:52 +0800

    a new leetcode test for hashtable

---
+++ HashTable/3.lengthOfLongestSubstring/lengthOfLongestSubstring.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <String.h>
+
+int lengthOfLongestSubstring(char * s){
+
+}
+
+void main(){
+    char* test = "asdasd";
+    printf("test len is %d\n",strlen(test));
+    printf("test len is %d\n",sizeof(test));
+    char test1[] = "asdasdsad";
+    printf("test1 len is %d\n",strlen(test1));
+    printf("test1 len is %d\n",sizeof(test1));
+}
+
