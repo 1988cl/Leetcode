4ddfc364c4511d4104bd069f02d4bc6c98d8dc59
chenl <18116491546@163.com>
2023-07-03 21:53:07 +0800

    update the length of longest substring but not the final version

--- HashTable/3.lengthOfLongestSubstring/lengthOfLongestSubstring.c
+++ HashTable/3.lengthOfLongestSubstring/lengthOfLongestSubstring.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <String.h>
+#include "uthash.h"
+
+typedef struct {
+    char key;
+    char* val;
+    UT_hash_handle hh;
+}stringItem,*stringItemP;
+
+stringItemP stringItems = NULL;
 
 int lengthOfLongestSubstring(char * s){
-
+    int index = 0;
+    int len = 1;
+    int result = 1;
+    while((*s)!=NULL){
+        int len = 1;
+        stringItemP tmp = NULL;
+        char ckey = *s;
+        HASH_FIND_STR(stringItems,&ckey,tmp);
+        if(tmp==NULL){
+            tmp = (stringItemP)malloc(sizeof(stringItem));
+            if(tmp==NULL){
+                exit(1);
+            }
+            tmp->key = ckey;
+            tmp->val = s;
+            HASH_ADD_STR(stringItems,key,tmp);
+        }
+    }
+    
+    return result;
 }
 
 void main(){
