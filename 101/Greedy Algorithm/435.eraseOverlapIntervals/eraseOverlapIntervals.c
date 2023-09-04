b7a71fd1e30309c4fc28553a267d5c157452a316
chenl <18116491546@163.com>
2023-09-04 21:06:32 +0800

    update the qsort and eraseOverlapIntervals

--- 101/Greedy Algorithm/435.eraseOverlapIntervals/eraseOverlapIntervals.c
+++ 101/Greedy Algorithm/435.eraseOverlapIntervals/eraseOverlapIntervals.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void arrSort1(int** arr,int left, int right){
+    if (left > right) {
+            return;
+        }
+        int pivot = arr[left][1];  // 定义第一个数为基准数
+
+        int i = left;
+        int j = right;
+
+        while (i < j) {
+            while (pivot <= arr[j][1] && i < j) {  // 从右往左找比基准数小的
+                j = j - 1;
+            }
+            while (pivot >= arr[i][1] && i < j) {  // 从左往右找比基准数大的
+                i = i + 1;
+            }
+            if (i < j) {
+                int temp[2];
+                temp[0] = arr[i][0];
+                temp[1] = arr[i][1];
+                arr[i][0] = arr[j][0];
+                arr[i][1] = arr[j][1];
+                arr[j][0] = temp[0];
+                arr[j][1] = temp[1];
+            }
+        }
+        arr[left][0] = arr[i][0];
+        arr[left][1] = arr[i][1];  // i位置的数一定小于基准数，两者可以进行交换
+        arr[i][1] = pivot;  // i位置为基准数的最终位置
+
+        arrSort1(arr, left, i-1);
+        arrSort1(arr, i+1, right);
+}
 
 void arrSort(int** arr,int N){
     int pivot;
@@ -22,9 +55,12 @@ void arrSort(int** arr,int N){
 int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize){
     int result = 0;
     arrSort(intervals,intervalsSize);
+    int pre = intervals[0][1];
     for(int i=1;i<intervalsSize;i++){
-        if(intervals[i][0]<intervals[i-1][1]){
+        if(intervals[i][0]<pre){
             result++;
+        }else{
+            pre = intervals[i][1];
         }
     }
     return result;   
@@ -32,13 +68,13 @@ int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColS
 
 void main(){
     //[[1,100],[11,22],[1,11],[2,12]] / [1,11] [2,12] [11,22] [1,100]
-    int arr[4][2] = {{1,2},{2,3},{3,4},{1,3}};
+    int arr[6][2] = {{1,2},{2,3},{3,4},{1,3},{5,8},{4,6}};
     int* arrP[4];
-    for(int i=0;i<4;i++){
+    for(int i=0;i<6;i++){
         arrP[i] = arr[i];
     } 
-    arrSort(arrP,4);
-    for(int i=0;i<4;i++){
+    arrSort1(arrP,0,5);
+    for(int i=0;i<6;i++){
         for(int j=0;j<2;j++){
             printf("[%d,%d]:{%d}\n",i,j,arr[i][j]);
         }
