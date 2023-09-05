e83646897564d028794449a5534c9cce1b3fe71c
1988cl <18116491546@163.com>
2023-09-05 13:45:59 +0800

    new version for eraseOverLapIntervals

--- 101/Greedy Algorithm/435.eraseOverlapIntervals/eraseOverlapIntervals.c
+++ 101/Greedy Algorithm/435.eraseOverlapIntervals/eraseOverlapIntervals.c
@@ -22,22 +22,27 @@ void arrSort(int** arr,int N){
 int eraseOverlapIntervals(int** intervals, int intervalsSize, int* intervalsColSize){
     int result = 0;
     arrSort(intervals,intervalsSize);
+    int pre = intervals[0][1];
     for(int i=1;i<intervalsSize;i++){
-        if(intervals[i][0]<intervals[i-1][1]){
+        if(intervals[i][0]<pre){
             result++;
+        }else{
+            pre = intervals[i][1];
         }
     }
     return result;   
 }
 
 void main(){
-    //[[1,100],[11,22],[1,11],[2,12]] / [1,11] [2,12] [11,22] [1,100]
+    //[[1,100],[11,22],[1,11],[2,12]] / [1,11] [2,12] [11,22] [1,100] / [2,10] [1,11] [10,11] [11,22] 
     int arr[4][2] = {{1,2},{2,3},{3,4},{1,3}};
     int* arrP[4];
     for(int i=0;i<4;i++){
         arrP[i] = arr[i];
     } 
+    
     arrSort(arrP,4);
+
     for(int i=0;i<4;i++){
         for(int j=0;j<2;j++){
             printf("[%d,%d]:{%d}\n",i,j,arr[i][j]);
